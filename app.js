7aa8457b6587349a1f618642552cba48fbf85360
shaheer <syedshaheer266@gmail.com>
2023-08-06 03:20:40 -0700

    complete

---
+++ app.js
@@ -0,0 +1,123 @@
+var typed= new  Typed(".multiple-text",{
+    strings:["Frontend Developer", "Web designer"],
+    typeSpeed:80,
+    backSpeed:80,
+    backDelay:1000,
+    loop: true
+})
+
+
+let btn=document.querySelector(".btn")
+let screen=document.querySelector(".projects")
+
+function load(){
+
+    screen.innerHTML+=`<div class="screen">
+    <div class="work">
+        <!-- <a href="https://syedshaheer01.github.io/Apple_website/" target="_blank"> -->
+            <img src="./Screenshot (1).png" alt="" width="300" height="150"></a>
+            <div class="layer">
+                <h3 style="color: white;">Apple Website</h3>
+                <p style="color: white;">apple page.</p>
+            <a href="https://syedshaheer01.github.io/Apple_website/" target="_blank"><i
+                class="fa-solid fa-up-right-from-square"></i></a>
+                
+            </div>
+            </div>
+                
+
+            <div class="work">
+        <!-- <a href="https://syedshaheer01.github.io/Apple_website/" target="_blank"> -->
+            <img src="./Screenshot (1).png" alt="" width="300" height="150"></a>
+            <div class="layer">
+                <h3 style="color: white;">Apple Website</h3>
+                <p style="color: white;">apple page.</p>
+            <a href="https://syedshaheer01.github.io/Apple_website/" target="_blank"><i
+                class="fa-solid fa-up-right-from-square"></i></a>
+                
+            </div>
+            </div>
+
+            <div class="work">
+            <!-- <a href="https://syedshaheer01.github.io/Apple_website/" target="_blank"> -->
+                <img src="./Screenshot (1).png" alt="" width="300" height="150"></a>
+                <div class="layer">
+                    <h3 style="color: white;">Apple Website</h3>
+                    <p style="color: white;">apple page.</p>
+                <a href="https://syedshaheer01.github.io/Apple_website/" target="_blank"><i
+                    class="fa-solid fa-up-right-from-square"></i></a>
+                    
+                </div>
+                </div>
+
+
+                <div class="work">
+                <!-- <a href="https://syedshaheer01.github.io/Apple_website/" target="_blank"> -->
+                    <img src="./Screenshot (1).png" alt="" width="300" height="150"></a>
+                    <div class="layer">
+                        <h3 style="color: white;">Apple Website</h3>
+                        <p style="color: white;">apple page.</p>
+                    <a href="https://syedshaheer01.github.io/Apple_website/" target="_blank"><i
+                        class="fa-solid fa-up-right-from-square"></i></a>
+                        
+                    </div>
+                    </div>
+    </div>
+    
+    <div class="screen">
+    <div class="work">
+        <!-- <a href="https://syedshaheer01.github.io/Apple_website/" target="_blank"> -->
+            <img src="./Screenshot (1).png" alt="" width="300" height="150"></a>
+            <div class="layer">
+                <h3 style="color: white;">Apple Website</h3>
+                <p style="color: white;">apple page.</p>
+            <a href="https://syedshaheer01.github.io/Apple_website/" target="_blank"><i
+                class="fa-solid fa-up-right-from-square"></i></a>
+                
+            </div>
+            </div>
+                
+
+            <div class="work">
+        <!-- <a href="https://syedshaheer01.github.io/Apple_website/" target="_blank"> -->
+            <img src="./Screenshot (1).png" alt="" width="300" height="150"></a>
+            <div class="layer">
+                <h3 style="color: white;">Apple Website</h3>
+                <p style="color: white;">apple page.</p>
+            <a href="https://syedshaheer01.github.io/Apple_website/" target="_blank"><i
+                class="fa-solid fa-up-right-from-square"></i></a>
+                
+            </div>
+            </div>
+
+            <div class="work">
+            <!-- <a href="https://syedshaheer01.github.io/Apple_website/" target="_blank"> -->
+                <img src="./Screenshot (1).png" alt="" width="300" height="150"></a>
+                <div class="layer">
+                    <h3 style="color: white;">Apple Website</h3>
+                    <p style="color: white;">apple page.</p>
+                <a href="https://syedshaheer01.github.io/Apple_website/" target="_blank"><i
+                    class="fa-solid fa-up-right-from-square"></i></a>
+                    
+                </div>
+                </div>
+
+
+                <div class="work">
+                <!-- <a href="https://syedshaheer01.github.io/Apple_website/" target="_blank"> -->
+                    <img src="./Screenshot (1).png" alt="" width="300" height="150"></a>
+                    <div class="layer">
+                        <h3 style="color: white;">Apple Website</h3>
+                        <p style="color: white;">apple page.</p>
+                    <a href="https://syedshaheer01.github.io/Apple_website/" target="_blank"><i
+                        class="fa-solid fa-up-right-from-square"></i></a>
+                        
+                    </div>
+                    </div>
+    </div>`
+    
+    
+    
+    
+console.log("hello")
+}
